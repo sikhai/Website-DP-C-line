98b6407a3ef5efd4a615fff7928247e819e4ebdb
sikhai <55691270+sikhai@users.noreply.github.com>
2024-06-06 14:57:46 +0700

    update

--- dpc-web/js/main.js
+++ dpc-web/js/main.js
@@ -4,3 +4,30 @@ var swiper = new Swiper(".proj_mouth", {
         prevEl: ".swiper-button-prev",
     },
 });
+// slide service
+var swiper = new Swiper(".services_list", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
