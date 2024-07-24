f458a8b37601dbaddb927a60aa412063e96fd6d7
sikhai <55691270+sikhai@users.noreply.github.com>
2024-07-24 14:06:20 +0700

    change type field value attributes

--- database/migrations/2024_07_04_072009_create_attributes_table.php
+++ database/migrations/2024_07_04_072009_create_attributes_table.php
@@ -10,7 +10,7 @@ class CreateAttributesTable extends Migration
     {
         Schema::create('attributes', function (Blueprint $table) {
             $table->id();
-            $table->string('value')->unique();
+            $table->text('value');
             $table->timestamps();
         });
     }
