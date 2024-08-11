3a6e02afb7ebdd7cffd7e7914d88c025b93750c3
unknown <hiep1998vv@gmail.com>
2024-08-11 22:56:22 +0700

    update 1108

---
+++ dpc-web/js/Scrollbanner.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Register the ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger);
+    
+    // Ensure the title is initially hidden and off-screen
+    gsap.set(".banner .content", {
+        opacity: 0,
+        y: 300
+    });
+
+    // Animate section 2 when scrolling up
+    gsap.to(".image-container", {
+    width: "1400px",
+    height: "860px",
+    scrollTrigger: {
+        trigger: ".banner",
+        start: "center center", // Animation starts as soon as the section enters the viewport
+        end: "bottom 70%", // Animation continues until the section is fully out of the viewport
+        pin: ".banner",
+        pinSpacing: false,
+        scrub: true, // Links the animation progress to the scroll position
+        markers: true // Optional: Show markers for debugging
+        }
+    });
+
+    // Animate section 3 with a different effect
+    // gsap.to('#section3', {
+    //     rotation: 360,
+    //     scrollTrigger: {
+    //         trigger: '#section3',
+    //         start: 'top 50%',
+    //         end: 'bottom 50%',
+    //         scrub: true,
+    //         markers: false // Optional, for debugging
+    //     }
+    // });
+
+    // Animation for the title appearing at 95% of the content2 expansion
+    // gsap.to("#section2 .title", {
+    //     opacity: 1,
+    //     y: 0,
+    //     duration: 1,
+    //     scrollTrigger: {
+    //         trigger: "#section2 .content2",
+    //         start: "top 95%",  // Trigger the animation when content2 has reached 95% of its target
+    //         toggleActions: "play none none none", // Only play the animation once
+    //         markers: true,     // Optional: Show markers for debugging
+    //         onEnter: () => gsap.to("#section2 .title", { opacity: 1, y: 0 }),  // Animate title into view
+    //         onLeaveBack: () => gsap.set("#section2 .title", { opacity: 0, y: 100 }), // Reset title when scrolling back
+    //     }
+    // });
+    // Animation for the title appearing at 95% of the content2 expansion
+    ScrollTrigger.create({
+        trigger: ".banner .image-container",
+        start: "top top",  // Trigger the animation when content2 has reached 95% of its target
+        onEnter: () => gsap.to(".banner .content", {
+            opacity: 1,
+            y: 0,
+            duration: 1
+        }),  // Animate title into view
+        onLeaveBack: () => gsap.set(".banner .content", {
+            opacity: 0,
+            y: 300
+        }), // Reset title when scrolling back
+        markers: true           // Optional: Show markers for debugging
+    });
+});
--- dpc-web/js/main.js
+++ dpc-web/js/main.js
@@ -1,168 +1,3 @@
-
-// swiper project
-var swiper = new Swiper(".proj_mouth", {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-// slide service
-var swiper = new Swiper(".services_list", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    speed: 2500,
-    loop: true,
-    // loopAdditionalSlides: 6,
-    autoplay: {
-        delay: 0, // 3 seconds
-        disableOnInteraction: false, // Continue autoplay even after user interactions
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 15,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 24,
-        },
-    },
-    on: {
-        slideChange: function () {
-          document.querySelectorAll('.overlay-service').forEach(function (overlay) {
-            overlay.style.opacity = '1';
-          });
-          var activeSlide = swiper.slides[swiper.activeIndex];
-          var overlay = activeSlide.querySelector('.overlay-service');
-          if (overlay) {
-            overlay.style.opacity = '0';
-          }
-
-          if (activeSlide.classList.contains('blank-slide')) {
-            // Move to the first real slide
-            swiper.slideToLoop(0, 0, false); // 0 is the first real slide, 0ms duration, false no transition
-            swiper.autoplay.stop();
-            // Restart autoplay after a short delay
-            setTimeout(function () {
-                swiper.autoplay.start();
-            }, 10000); 
-        }
-        },
-        
-    }
-});
-// Initially hide overlay for the first active slide
-var initialActiveSlide = swiper.slides[swiper.activeIndex];
-var initialOverlay = initialActiveSlide.querySelector('.overlay-service');
-if (initialOverlay) {
-  initialOverlay.style.opacity = '0';
-}
-
-
-// document.addEventListener('scroll', function() {
-//     const container = document.querySelector('.container-project');
-//     const middleImage = document.querySelector('.middle');
-
-//     // Calculate scroll percentage
-//     let scrollPosition = window.scrollY;
-//     let windowHeight = window.innerHeight;
-//     let bodyHeight = document.body.offsetHeight;
-
-//     let scrollPercentage = (scrollPosition / (bodyHeight - windowHeight)) * 100;
-
-//     if (scrollPercentage > 10) {
-//         container.classList.add('scrolled');
-//     } else {
-//         container.classList.remove('scrolled');
-//     }
-// });
-
-//banner annimation
-// document.addEventListener('scroll', function() {
-//     const banner = document.querySelector('.banner');
-//     const zoomImages = document.querySelectorAll('#zoomImage');
-//     const content = document.querySelectorAll('.content');
-//     const nextButton = document.querySelector('.swiper-button-next');
-//     const prevButton = document.querySelector('.swiper-button-prev');
-    
-//     const scrollPosition = window.scrollY;
-//     const maxScroll = 700; // maximum scroll area
-
-//     if (scrollPosition <= maxScroll) {
-//         const scale = 0.3 + (scrollPosition / maxScroll) * 0.7;
-//         zoomImages.forEach(img => img.style.transform = `scale(${scale})`);
-        
-//         if (scrollPosition >= 0.95*maxScroll) {
-//             banner.classList.add('zoomed');
-//             banner.classList.add('revealed');
-//         } else {
-//             banner.classList.remove('revealed');
-//             if (scrollPosition < maxScroll * 0.7) {
-//                 banner.classList.remove('zoomed');
-//             }
-//         }
-//     }
-// });
-
-window.addEventListener('scroll', () => {
-    const section_ = document.getElementById('image-section');
-    const imageContainers = document.querySelectorAll('.image-container');
-    const sectionRect = section_.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-  
-    if (sectionRect.top <= windowHeight / 2 && sectionRect.bottom >= windowHeight / 2) {
-        const scrollPercentage = (windowHeight / 2 - sectionRect.top) / sectionRect.height;
-    
-        // Expand the image container based on scroll percentage
-        const maxWidth = 1400;
-        const maxHeight = 860;
-        const initialWidth = 420;
-        const initialHeight = 280;
-    
-        const newWidth = initialWidth + (maxWidth - initialWidth) * scrollPercentage;
-        const newHeight = initialHeight + (maxHeight - initialHeight) * scrollPercentage;
-        imageContainers.forEach((imageContainer) => {
-            imageContainer.style.position = 'fixed';
-            imageContainer.style.width = `${newWidth}px`;
-            imageContainer.style.height = `${newHeight}px`;
-            imageContainer.style.top = '50%'; // Always center the image vertically
-            imageContainer.style.left = '50%'; // Always center the image horizontally
-            imageContainer.style.transform = 'translate(-50%, -50%)';
-            // console.log("1");
-        })
-    } else if (sectionRect.bottom < windowHeight) {
-        // Keep the image in place when reaching the end of section 2
-        imageContainers.forEach((imageContainer) => {
-            imageContainer.style.position = 'fixed';
-            imageContainer.style.top = '50%';
-            imageContainer.style.transform = 'translate(-50%, -50%)';
-        })
-    } else {
-        // Before reaching section 2
-        imageContainers.forEach((imageContainer) => {
-            imageContainer.style.width = '420px';
-            imageContainer.style.height = '280px';
-            imageContainer.style.position = 'absolute';
-            imageContainer.style.top = '50%';
-            imageContainer.style.left = '50%';
-            imageContainer.style.transform = 'translate(-50%, -50%)';
-        })
-    }
-  });
-  
-  
 //project annimantion
 const images = document.querySelectorAll('.img-wrapper');
 const imgArea = document.querySelector('.img-area');
@@ -222,5 +57,176 @@ function startFlipping() {
 }
 
 startFlipping();
+
+
+
+// swiper project
+// var swiper = new Swiper(".proj_mouth", {
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+// slide service
+
+var swiperConfig = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    speed: 2500,
+    loop: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+    on: {
+        slideChange: function () {
+            document.querySelectorAll('.overlay_service').forEach(function (overlay) {
+                overlay.style.opacity = '1';
+            });
+            // Đảm bảo swiper được khởi tạo trước khi sử dụng
+            if (typeof swiper !== 'undefined') {
+                var activeSlide = swiper.slides[swiper.activeIndex];
+                var overlay = activeSlide.querySelector('.overlay_service');
+                if (overlay) {
+                    overlay.style.opacity = '0';
+                }
+
+                if (activeSlide.classList.contains('blank-slide')) {
+                    swiper.slideToLoop(0, 0, false);
+                    swiper.autoplay.stop();
+                    setTimeout(function () {
+                        swiper.autoplay.start();
+                    }, 10000);
+                }
+            }
+        },
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper(".swiper.services_list", swiperConfig);
+});
+
+// document.addEventListener('scroll', function() {
+//     const container = document.querySelector('.container-project');
+//     const middleImage = document.querySelector('.middle');
+
+//     // Calculate scroll percentage
+//     let scrollPosition = window.scrollY;
+//     let windowHeight = window.innerHeight;
+//     let bodyHeight = document.body.offsetHeight;
+
+//     let scrollPercentage = (scrollPosition / (bodyHeight - windowHeight)) * 100;
+
+//     if (scrollPercentage > 10) {
+//         container.classList.add('scrolled');
+//     } else {
+//         container.classList.remove('scrolled');
+//     }
+// });
+
+//banner annimation
+// document.addEventListener('scroll', function() {
+//     const banner = document.querySelector('.banner');
+//     const zoomImages = document.querySelectorAll('#zoomImage');
+//     const content = document.querySelectorAll('.content');
+//     const nextButton = document.querySelector('.swiper-button-next');
+//     const prevButton = document.querySelector('.swiper-button-prev');
+    
+//     const scrollPosition = window.scrollY;
+//     const maxScroll = 700; // maximum scroll area
+
+//     if (scrollPosition <= maxScroll) {
+//         const scale = 0.3 + (scrollPosition / maxScroll) * 0.7;
+//         zoomImages.forEach(img => img.style.transform = `scale(${scale})`);
+        
+//         if (scrollPosition >= 0.95*maxScroll) {
+//             banner.classList.add('zoomed');
+//             banner.classList.add('revealed');
+//         } else {
+//             banner.classList.remove('revealed');
+//             if (scrollPosition < maxScroll * 0.7) {
+//                 banner.classList.remove('zoomed');
+//             }
+//         }
+//     }
+// });
+
+// window.addEventListener('scroll', () => {
+//     const section_ = document.querySelector('.banner');
+//     const imageContainers = document.querySelectorAll('.image-container');
+//     const sectionRect = section_.getBoundingClientRect();
+//     const windowHeight = window.innerHeight;
+  
+//     if (sectionRect.top <= windowHeight / 2 && sectionRect.bottom >= windowHeight / 2) {
+//         const scrollPercentage = (windowHeight / 2 - sectionRect.top) / sectionRect.height;
+    
+//         // Expand the image container based on scroll percentage
+//         const maxWidth = 1400;
+//         const maxHeight = 860;
+//         const initialWidth = 420;
+//         const initialHeight = 280;
+    
+//         const newWidth = initialWidth + (maxWidth - initialWidth) * scrollPercentage;
+//         const newHeight = initialHeight + (maxHeight - initialHeight) * scrollPercentage;
+//         imageContainers.forEach((imageContainer) => {
+//             imageContainer.style.position = 'fixed';
+//             imageContainer.style.width = `${newWidth}px`;
+//             imageContainer.style.height = `${newHeight}px`;
+//             imageContainer.style.top = '50%'; // Always center the image vertically
+//             imageContainer.style.left = '50%'; // Always center the image horizontally
+//             imageContainer.style.transform = 'translate(-50%, -50%)';
+//             // console.log("1");
+//             console.log("Image container position:", imageContainer.style.top, imageContainer.style.left);
+//         })
+//     } else if (sectionRect.bottom < windowHeight) {
+//         // Keep the image in place when reaching the end of section 2
+//         imageContainers.forEach((imageContainer) => {
+//             imageContainer.style.position = 'fixed';
+//             imageContainer.style.top = '50%';
+//             imageContainer.style.transform = 'translate(-50%, -50%)';
+//             console.log("Image container position:", imageContainer.style.top, imageContainer.style.left);
+
+//         })
+//     } else {
+//         // Before reaching section 2
+//         imageContainers.forEach((imageContainer) => {
+//             imageContainer.style.width = '420px';
+//             imageContainer.style.height = '280px';
+//             imageContainer.style.position = 'absolute';
+//             imageContainer.style.top = '50%';
+//             imageContainer.style.left = '50%';
+//             imageContainer.style.transform = 'translate(-50%, -50%)';
+//             console.log("Image container position:", imageContainer.style.top, imageContainer.style.left);
+
+//         })
+//     }
+    
+//   });
+  
+  
+
 
 
