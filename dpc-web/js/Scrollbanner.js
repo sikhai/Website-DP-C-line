4b29a0b7df4c35e58af2d8ed3cc8c2399354d107
sikhai <55691270+sikhai@users.noreply.github.com>
2024-08-26 20:41:42 +0700

    updaste

--- dpc-web/js/Scrollbanner.js
+++ dpc-web/js/Scrollbanner.js
@@ -9,29 +9,32 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Animate section 2 when scrolling up
-    gsap.to(".image-container", {
-    width: "1400px",
-    height: "860px",
-    scrollTrigger: {
-        trigger: ".banner",
-        start: "center center", // Animation starts as soon as the section enters the viewport
-        end: "bottom 70%", // Animation continues until the section is fully out of the viewport
-        pin: ".banner",
-        pinSpacing: false,
-        scrub: true, // Links the animation progress to the scroll position
-        markers: true // Optional: Show markers for debugging
-        }
-    });
+    // gsap.to(".image-container", {
+    // width: "1400px",
+    // height: "860px",
+    // scrollTrigger: {
+    //     trigger: ".banner",
+    //     start: "center center",
+    //     end: "bottom 70%",
+    //     pin: ".banner",
+    //     pinSpacing: false,
+    //     scrub: true,
+    //     markers: true 
+    //     }
+    // });
 
-    // ------------------phase1
-    // gsap.to(".banner .surround-img:nth-child(1)", {
-    //     y: "-300px",
+    // // ------------------phase1
+    // gsap.to(".banner .surrounding-top-images img", {
+    //     width: "1406px",
+    //     height: "673px",
     //     scrollTrigger: {
-    //         trigger: ".banner .image-container",
+    //         trigger: ".banner .overlay",
     //         start: "top center",
-    //         end: "center center",
+    //         end: "bottom 70%",
+    //         pin: ".banner",
+    //         pinSpacing: false,
     //         scrub: true,
-    //         markers: true
+    //         markers: true 
     //     }
     // });
 
@@ -143,18 +146,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // });
    
     // Animation for the title appearing at 95% of the content2 expansion
-    ScrollTrigger.create({
-        trigger: ".banner .image-container",
-        start: "top top",  // Trigger the animation when content2 has reached 95% of its target
-        onEnter: () => gsap.to(".banner .content", {
-            opacity: 1,
-            y: 0,
-            duration: 1
-        }),  // Animate title into view
-        onLeaveBack: () => gsap.set(".banner .content", {
-            opacity: 0,
-            y: 300
-        }), // Reset title when scrolling back
-        markers: true           // Optional: Show markers for debugging
-    });
+    // ScrollTrigger.create({
+    //     trigger: ".banner .image-container",
+    //     start: "top top",  // Trigger the animation when content2 has reached 95% of its target
+    //     onEnter: () => gsap.to(".banner .content", {
+    //         opacity: 1,
+    //         y: 0,
+    //         duration: 1
+    //     }),  // Animate title into view
+    //     onLeaveBack: () => gsap.set(".banner .content", {
+    //         opacity: 0,
+    //         y: 300
+    //     }), // Reset title when scrolling back
+    //     markers: false
+    // });
 });
