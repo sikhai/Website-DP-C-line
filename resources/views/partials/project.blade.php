ecdcf5563af2f60e7266d424dfd8d99001b6db48
Phạm Sỹ Khải <khai.pham>
2024-11-06 17:42:42 +0700

    update

--- resources/views/partials/project.blade.php
+++ resources/views/partials/project.blade.php
@@ -5,16 +5,18 @@
             Projects we are proud of
         </h2>
     </div>
-    <div class="d-flex img-area">
-        @foreach ($projects_home as $key => $item)
-            @php
-                $images = json_decode($item['images'], true);
-            @endphp
-            <div class="img-wrapper image{{ $key + 1 }}">
-                <img src="{{ Voyager::image($images[0]) }}" alt="{{ $item['project_name'] }}" loading="lazy"
-                    class="hover-image ">
-                <div class="overlay"></div>
-            </div>
-        @endforeach
-    </div>
+    <a href="./our-project">
+        <div class="d-flex img-area">
+            @foreach ($projects_home as $key => $item)
+                @php
+                    $images = json_decode($item['images'], true);
+                @endphp
+                <div class="img-wrapper image{{ $key + 1 }}">
+                    <img src="{{ Voyager::image($images[0]) }}" alt="{{ $item['project_name'] }}" loading="lazy"
+                        class="hover-image ">
+                    <div class="overlay"></div>
+                </div>
+            @endforeach
+        </div>
+    </a>
 </section>
