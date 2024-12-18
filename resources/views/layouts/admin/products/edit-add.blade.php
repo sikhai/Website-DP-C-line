0fafd65cee8eb7e52cf7c4b0784d27a3ca7ffda0
Phạm Sỹ Khải <khai.pham>
2024-12-18 16:43:14 +0700

    add image product admin

--- resources/views/layouts/admin/products/edit-add.blade.php
+++ resources/views/layouts/admin/products/edit-add.blade.php
@@ -319,7 +319,24 @@
                     <!-- ### IMAGE ### -->
                     <div class="panel panel-bordered panel-primary">
                         <div class="panel-heading">
-                            <h3 class="panel-title"><i class="icon wb-image"></i> {{ __('voyager::post.image') }}</h3>
+                            <h3 class="panel-title"><i class="icon wb-image"></i> {{ __('voyager::product.avatar') }}</h3>
+                            <div class="panel-actions">
+                                <a class="panel-action voyager-angle-down" data-toggle="panel-collapse"
+                                    aria-hidden="true"></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            @if (isset($dataTypeContent->image))
+                                <img src="{{ filter_var($dataTypeContent->image, FILTER_VALIDATE_URL) ? $dataTypeContent->image : Voyager::image( $dataTypeContent->image ) }}" style="width:100%" />
+                            @endif
+                            <input type="file" name="image">
+                        </div>
+                    </div>
+
+                    <!-- ### IMAGE ### -->
+                    <div class="panel panel-bordered panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><i class="icon wb-image"></i> {{ __('voyager::product.image') }}</h3>
                             <div class="panel-actions">
                                 <a class="panel-action voyager-angle-down" data-toggle="panel-collapse"
                                     aria-hidden="true"></a>
