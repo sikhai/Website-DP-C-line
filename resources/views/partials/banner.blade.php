d2d9e35ffde2ab334f792b32f2b95780c7413e91
Phạm Sỹ Khải <khai.pham>
2025-02-24 15:39:25 +0700

    update css mobile home  page

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,21 +1,3 @@
-{{-- <section class="banner" id="section2">
-    <div class="image-container">
-        <img src="images/spacejoy-YI2YkyaREHk-unsplash.jpg" alt="">
-        <div class="overlay"></div>
-    </div>
-    <!-- <div class="surrounding-images">
-        <img src="images/component1.jpg" class="surround-img" alt="">
-        <img src="images/component2.jpg" class="surround-img" alt="">
-        <img src="images/component3.jpg" class="surround-img" alt="">
-        <img src="images/component4.jpg" class="surround-img" alt="">
-        <img src="images/component5.jpg" class="surround-img" alt="">
-    </div> -->
-    <div class="content" >
-        <p>PROJECT OF THE MONTH</p>
-        <h3>JW MARRIOT PHU QUOC RESORT</h3>
-    </div>
-</section> --}}
-
 <section class="banner">
     <div class="image-container">
         <img src="images/spacejoy-YI2YkyaREHk-unsplash.jpg" alt="">
@@ -30,6 +12,8 @@
 
     <div class="content">
         <p>PROJECT OF THE MONTH</p>
-        <h3>JW MARRIOT PHU QUOC RESORT</h3>
+        <a href="/our-project/{{ $projects_of_mounth['slug'] }}">
+            <h3>{{ $projects_of_mounth['name'] }}</h3>
+        </a>
     </div>
 </section>
