9ac329f10b7a20e0c516241044b5e60b1a462e6e
sikhai <55691270+sikhai@users.noreply.github.com>
2025-05-30 21:24:31 +0700

    delect front-end hiệp

--- public/dpc-web/js/loader.js
+++
@@ -1,9 +0,0 @@
-//loader
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        document.querySelector('.loader').style.display = 'none';
-        const layout1 = document.querySelector('.layout');
-        layout1.classList.remove('hidden');
-        layout1.style.opacity = '1';
-    }, 500); // 2 seconds delay
-});
