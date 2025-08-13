3a667b979532d841f91b2a146e62e71830584852
Phạm Sỹ Khải <khai.pham>
2025-08-13 17:34:38 +0700

    add vận chuyển trong nước

--- app/Helpers/DeliveryVendorHelper.php
+++ app/Helpers/DeliveryVendorHelper.php
@@ -9,10 +9,8 @@ class DeliveryVendorHelper
     public function getSuppliers()
     {
         return DeliveryVendor::whereHas('freightRates.freightType', function ($query) {
-            $query->where('type', '!=', 'domestic');
         })->with(['freightRates' => function ($query) {
             $query->whereHas('freightType', function ($q) {
-                $q->where('type', '!=', 'domestic');
             });
         }])->get();
     }
